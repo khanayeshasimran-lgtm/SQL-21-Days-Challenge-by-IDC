d4b16634af3174e473b47ea76edfefef03533708
Ayesha Simran <khanayeshasimran@gmail.com>
2025-11-03 17:18:29 +0530

    Add Day1 challenge SQL queries
    Added SQL queries for retrieving patient and service data.

---
+++ Day1-Challenge.sql
@@ -0,0 +1,13 @@
+--1. Retrieve all columns from the patients' table.
+select * from patients;
+
+--2. Select only the "patient_id, name, and age" columns from the 'patients' table.
+select patient_id,name,age
+from patients;
+
+--3. Display the first 10 records from the 'services_weekly' table.
+select * from services_weekly limit 10;
+
+--Day1 challenge
+SELECT distinct service 
+FROM patients ;
